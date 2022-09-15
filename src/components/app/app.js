aafad4bd1ee533ab3b425ab4c56687c864a38563
artemBodryi <artembodry@gmail.com>
2022-09-15 13:58:30 +0300

    create logic on "delete" btn (todo-list-item -> todo-list -> app)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,18 +1,36 @@
-import {React} from "react";
+import {React, Component} from "react";
 
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
 import ItemStatusFilter from "../item-status-filter";
 import TodoList from "../todo-list/todo-list";
 
-const App = () => {
+export default class App extends Component {
 
-    const todoData = [
+  state = {
+    todoData: [
       {label: 'Drink Coffee', important: false, id: 1 },
       {label: 'Make Awesome App', important: true, id: 2 },
       {label: 'Have a lunch', important: false, id: 3 },
-    ];
-  
+    ]
+  };
+
+  deleteItem = (id) => {
+    this.setState(({ todoData }) =>{
+      const idx = todoData.findIndex((el) => el.id === id);
+
+      const newArr = [
+        ...todoData.slice(0, idx), 
+        ...todoData.slice(idx + 1)
+      ];
+
+      return {
+        todoData: newArr
+      };
+    });
+  };
+
+  render() {
     return(
       <div className="todo-app">
         <AppHeader toDo={1} done={3}/>
@@ -20,11 +38,11 @@ const App = () => {
           <SearchPanel />
           <ItemStatusFilter />
         </div>
-        
-        <TodoList todos = { todoData }/>
+          
+        <TodoList 
+        todos = { this.state.todoData }
+        onDeleted={ this.deleteItem }/>
       </div>
     );
-  };
-
-
-export default App;
+  }
+};
