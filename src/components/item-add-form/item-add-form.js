21b280c7eec3c8b9b5979a65c1d75dd7640c893a
artemBodryi <artembodry@gmail.com>
2022-09-16 13:20:45 +0300

    -was added input for new tasks (with logic)

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -2,15 +2,38 @@ import { React, Component } from 'react';
 import './item-add-form.css'
 
 export default class ItemAddForm extends Component {
+
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onCreateItem(this.state.label)
+    }
     render() {
         return (
-            <div className="item-add-form">
-                <button 
-                    className="btn btn-outline-secondary"
-                    onClick={() => this.props.onCreateItem(`Hello Wold`)}>
-                    Add Item
-                </button>
-            </div>
-        )
+           <>
+            <form
+                className="item-add-form d-flex"
+                onSubmit={this.onSubmit}>
+
+                <input type="text"
+                       className="form-control"
+                       value={this.state.label}
+                       onChange={this.onLabelChange}
+                       placeholder="What needs to be done?" />
+
+            <button type="submit"
+                    className="btn btn-outline-secondary">Add</button>
+            </form>
+           </> 
+        );
     }
 }
